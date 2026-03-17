a89e9e02a53ce7916c1604b9fcb63d0ea50dbc3d
Bobson Latonio <95969767+BOBLATONIO@users.noreply.github.com>
2026-03-17 12:00:37 +0800

    Add files via upload
    add scipt

---
+++ pcinfo.ps1
@@ -0,0 +1,20 @@
+$mb=Get-CimInstance Win32_BaseBoard
+$cpu=(Get-CimInstance Win32_Processor).Name
+$ram = [math]::Round((Get-CimInstance Win32_PhysicalMemory | Measure-Object Capacity -Sum).Sum / 1GB, 0)
+$os=(Get-CimInstance Win32_OperatingSystem).Caption
+$gpu=(Get-CimInstance Win32_VideoController).Name
+
+$disks=Get-PhysicalDisk|ForEach-Object{
+($_.FriendlyName+' '+$_.MediaType+' '+[math]::Round($_.Size/1GB)+'GB')
+}
+
+Invoke-RestMethod -Uri "https://script.google.com/macros/s/AKfycbyXWSw2naSk5ksYobMlb7JOPaixMF24uN_239L4ZpsBX7Z87EsCyoctrLMlN_KoFe87rQ/exec" -Method POST -Body @{
+pc=$env:COMPUTERNAME
+mbbrand=$mb.Manufacturer
+mbmodel=$mb.Product
+cpu=$cpu
+ram=$ram
+storage=($disks -join ' | ')
+gpu=$gpu
+windows=$os
+}
