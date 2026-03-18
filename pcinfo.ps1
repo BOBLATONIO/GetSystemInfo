8502e62c98c8308597143059d97f66dcddb33bff
Bobson Latonio <95969767+BOBLATONIO@users.noreply.github.com>
2026-03-18 14:14:25 +0800

    Add device type selection to PC info script

--- pcinfo.ps1
+++ pcinfo.ps1
@@ -1,24 +1,37 @@
-$FullName=Read-Host "Fullname"
-$PropertyNumber=Read-Host "Property No."
-$mb=Get-CimInstance Win32_BaseBoard
-$cpu=(Get-CimInstance Win32_Processor).Name
-$ram = [math]::Round((Get-CimInstance Win32_PhysicalMemory | Measure-Object Capacity -Sum).Sum / 1GB, 0)
-$os=(Get-CimInstance Win32_OperatingSystem).Caption
-$gpu=(Get-CimInstance Win32_VideoController).Name
+$FullName = Read-Host "Fullname"
+$PropertyNumber = Read-Host "Property No."
 
-$disks=Get-PhysicalDisk|ForEach-Object{
-($_.FriendlyName+' '+$_.MediaType+' '+[math]::Round($_.Size/1GB)+'GB')
+# Add choice
+$choice = Read-Host "Select Device Type [1]Laptop [2]Desktop:"
+
+if ($choice -eq "1") {
+    $deviceType = "Laptop"
+} elseif ($choice -eq "2") {
+    $deviceType = "Desktop"
+} else {
+    $deviceType = "Unknown"
+}
+
+$mb = Get-CimInstance Win32_BaseBoard
+$cpu = (Get-CimInstance Win32_Processor).Name
+$ram = [math]::Round((Get-CimInstance Win32_PhysicalMemory | Measure-Object Capacity -Sum).Sum / 1GB, 0)
+$os = (Get-CimInstance Win32_OperatingSystem).Caption
+$gpu = (Get-CimInstance Win32_VideoController).Name
+
+$disks = Get-PhysicalDisk | ForEach-Object {
+    ($_.FriendlyName + ' ' + $_.MediaType + ' ' + [math]::Round($_.Size / 1GB) + 'GB')
 }
 
 Invoke-RestMethod -Uri "https://script.google.com/a/macros/deped.gov.ph/s/AKfycbxIqOkQz6kjZdg6eFIoZSLN5Y9vHVxojEHebb2eOijuFFlUF3qb6aJzN5u2Sx8IXzzdRg/exec" -Method POST -Body @{
-fullname = $FullName
-propertynumber = $PropertyNumber
-pc=$env:COMPUTERNAME
-mbbrand=$mb.Manufacturer
-mbmodel=$mb.Product
-cpu=$cpu
-ram=$ram
-storage=($disks -join ' | ')
-gpu=$gpu
-windows=$os
+    fullname = $FullName
+    propertynumber = $PropertyNumber
+    devicetype = $deviceType 
+    pc = $env:COMPUTERNAME
+    mbbrand = $mb.Manufacturer
+    mbmodel = $mb.Product
+    cpu = $cpu
+    ram = $ram
+    storage = ($disks -join ' | ')
+    gpu = $gpu
+    windows = $os
 }
